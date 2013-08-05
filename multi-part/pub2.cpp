7bda918de6be1fad12629e4d2b751c41daf7fef3
ugovaretto <uvaretto@cscs.ch>
2013-08-05 16:59:18 +0200

    Added functions to automatically send/receive multipart messages stored in message arrays

---
+++ multi-part/pub2.cpp
@@ -0,0 +1,65 @@
+//Remote logger server: logs messages by publishing its process id and
+//the message content; remote clients subscribe to log output through a
+//broker
+//Author: Ugo Varetto
+
+//Note: UNIX only; for windows use DWORD type instead of pid_t and
+//GetProcessId instead of getpid
+
+#include <cassert>
+#include <cstring>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+//for framework builds on Mac OS:
+#ifdef __APPLE__
+#include <ZeroMQ/zmq.h>
+#else 
+#include <zmq.h>
+#endif
+#include "send-recv.h"
+
+typedef pid_t PID;
+
+
+//------------------------------------------------------------------------------
+PID get_proc_id() {
+    return getpid();
+}
+
+//------------------------------------------------------------------------------
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        std::cout << "usage: " 
+                  << argv[0] 
+                  << " <broker URI>"
+                  << std::endl;
+        std::cout << "Example: logger \"tcp://logbroker:5555\"\n";          
+        return 0;          
+    }
+    void* ctx = zmq_ctx_new(); 
+    void* req = zmq_socket(ctx, ZMQ_PUB);
+    const char* brokerURI = argv[1];
+    int rc = zmq_connect(req, brokerURI);
+    assert(rc == 0);
+    unsigned char buffer[0x100];
+    size_t size = 0;
+    std::cout << "PID: " << get_proc_id() << std::endl;
+    int pid = int(get_proc_id());
+    while(1) {
+        std::vector< std::vector< char > > msgs;
+        std::vector< char > msg1((char*) &pid, ((char*) &pid) + sizeof(pid));
+        char h[] = "hello";
+        std::vector< char > msg2(h, h + strlen(h));
+        msgs.push_back(msg1);
+        msgs.push_back(msg2);
+    	send_messages(req, msgs);
+        sleep(2);
+    }
+    rc = zmq_close(req);
+    assert(rc == 0);
+    rc = zmq_ctx_destroy(ctx);
+    assert(rc == 0);
+    return 0;
+}
+
---
+++ multi-part/send-recv.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+//------------------------------------------------------------------------------
+inline std::vector< std::vector< char > >
+recv_messages(void* socket) {
+    int rc = -1;
+    bool finished = false;
+    int opt = 0;
+    size_t len = sizeof(opt);
+    std::vector< std::vector< char > > ret;
+    std::vector< char > buffer(0x100);
+    rc = zmq_recv(socket, &buffer[0], buffer.size(), 0);
+    if(rc < 0) return ret;
+    buffer.resize(rc);
+    ret.push_back(buffer);
+    buffer.resize(0x100);
+    while(true) {
+        opt = 0;
+        if(zmq_getsockopt(socket, ZMQ_RCVMORE, &opt, &len) != 0) {
+			std::cerr << zmq_strerror(errno) << std::endl;
+			return ret;
+		}
+        if(opt) {
+        	rc = zmq_recv(socket, &buffer[0], buffer.size(), 0);
+        	if(rc < 0) break;
+            buffer.resize(rc);
+            ret.push_back(buffer);
+        } else break;
+    }
+   return ret;
+}
+//------------------------------------------------------------------------------
+inline void send_messages(void* socket,
+              const std::vector< std::vector< char > >& msgs) {
+   std::for_each(msgs.begin(), --msgs.end(), 
+                [socket](const std::vector< char >& msg){
+       const int rc = zmq_send(socket, &msg[0], msg.size(), ZMQ_SNDMORE);
+       assert(rc == msg.size());  
+   });
+   const int rc = zmq_send(socket, &(msgs.back()[0]), msgs.back().size(), 0);
+   assert(rc == msgs.back().size());
+}
