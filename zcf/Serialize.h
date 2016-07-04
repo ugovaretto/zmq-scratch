7ea2d23d0aa9bb9ac82dc2953bf44e83d2bd44eb
ugovaretto <ugovaretto@gmail.com>
2016-07-04 15:00:28 +0200

    Added serializer test

--- zcf/Serialize.h
+++ zcf/Serialize.h
@@ -12,10 +12,14 @@ using ByteArray = std::vector< Byte >;
 using ByteIterator = ByteArray::iterator;
 using ConstByteIterator = ByteArray::const_iterator;
 
+
+template < typename T >
+struct GetSerializer;
+
 //definitions
 template < typename T >
 struct SerializePOD {
-    static ByteArray Pack(const T &d, ByteArray buf) {
+    static ByteArray Pack(const T &d, ByteArray buf = ByteArray()) {
         const size_t sz = buf.size();
         buf.resize(buf.size() + sizeof(d));
         memmove(buf.data() + sz, &d, sizeof(d));
@@ -33,10 +37,10 @@ struct SerializePOD {
 
 template < typename T >
 struct Serialize {
-    static ByteArray Pack(const T &d, ByteArray buf) {
+    static ByteArray Pack(const T &d, ByteArray buf = ByteArray()) {
         const size_t sz = buf.size();
         buf.resize(buf.size() + sizeof(d));
-        new (buf.data() + size) T(d); //copy constructor
+        new (buf.data() + sz) T(d); //copy constructor
         return buf;
     }
     static ByteIterator Pack(const T &d, ByteIterator i) {
@@ -44,7 +48,7 @@ struct Serialize {
         return i + sizeof(d);
     }
     static ConstByteIterator UnPack(ConstByteIterator i, T& d) {
-        d = *reiterpret_cast< const T* >(i); //assignment operator
+        d = *reinterpret_cast< const T* >(&*i); //assignment operator
         return i + sizeof(T);
     }
 };
@@ -52,24 +56,23 @@ struct Serialize {
 
 template < typename T >
 struct SerializeVectorPOD {
-    static ByteArray Pack(const std::vector< T >& d, ByteArray buf) {
+    using ST = typename std::vector< T >::size_type;
+    static ByteArray Pack(const std::vector< T >& d,
+                          ByteArray buf = ByteArray()) {
         const size_t sz = buf.size();
-        using ST = std::vector< T >::size_type;
-        const ST s = d.size():
+        const ST s = d.size();
         buf.resize(buf.size() + sizeof(T) * d.size() + sizeof(ST));
-        memmove(buf.data + sz, &s, sizeof(s));
-        memmove(buf.data() + sz + sizeof(SZ), &d, sizeof(T) * d.size());
+        memmove(buf.data() + sz, &s, sizeof(s));
+        memmove(buf.data() + sz + sizeof(ST), &d, sizeof(T) * d.size());
         return buf;
     }
     static ByteIterator Pack(const std::vector< T > &d, ByteIterator i) {
-        using ST = std::vector< T >::size_type;
-        const ST s = d.size():
-        memmove(i, &s, sizeof(s));
-        memmove(i + sizeof(s), d.data(), d.size() * sizeof(T));
+        const ST s = d.size();
+        memmove(&*i, &s, sizeof(s));
+        memmove(&*i + sizeof(s), d.data(), d.size() * sizeof(T));
         return i + sizeof(T) * d.size() + sizeof(s);
     }
     static ConstByteIterator UnPack(ConstByteIterator i, std::vector< T >& d) {
-        using ST = std::vector< T >::size_type;
         ST s = 0;
         memmove(&s, &*i, sizeof(s));
         d.resize(s);
@@ -78,36 +81,34 @@ struct SerializeVectorPOD {
     }
 };
 
+
 template < typename T >
 struct SerializeVector {
-    static ByteArray Pack(const std::vector< T >& d, ByteArray buf) {
+    using ST = typename std::vector< T >::size_type;
+    using TS = typename GetSerializer< T >::Type;
+    static ByteArray Pack(const std::vector< T >& d,
+                          ByteArray buf = ByteArray()) {
         const size_t sz = buf.size();
-        using ST = std::vector< T >::size_type;
         const ST s = d.size();
         buf.resize(buf.size() + sizeof(T) * d.size() + sizeof(ST));
-        memmove(buf.data + sz, &s, sizeof(s));
-        using TS = GetSerializer< T >::Type;
+        memmove(buf.data() + sz, &s, sizeof(s));
         for(decltype(cbegin(d)) i = cbegin(d); i != cend(d); ++i) {
             buf = TS::Pack(*i, buf);
         }
         return buf;
     }
     static ByteIterator Pack(const std::vector< T > &d, ByteIterator bi) {
-        using ST = std::vector< T >::size_type;
         const ST s = d.size();
         memmove(bi, &s, sizeof(s));
-        using TS = GetSerializer< T >::Type;
         for(decltype(cbegin(d)) i = cbegin(d); i != cend(d); ++i) {
-            bi = TS::Pack(*i, buf);
+            bi = TS::Pack(*i, bi);
         }
         return bi;
     }
     static ConstByteIterator UnPack(ConstByteIterator bi, std::vector< T >& d) {
-        using ST = std::vector< T >::size_type;
         ST s = 0;
-        memmove(&s, &*i, sizeof(s));
+        memmove(&s, &*bi, sizeof(s));
         d.reserve(s);
-        using TS = GetSerializer< T >::Type;
         for(ST  i = 0; i != s; ++i) {
             T data;
             bi = TS::UnPack(bi, data);
@@ -118,16 +119,18 @@ struct SerializeVector {
 };
 
 struct SerializeString {
-    using V = std::vector< std::string::value_type >;
-    static ByteArray Pack(const std::string& d, ByteArray buf) {
-        return SerializeVectorPOD::Pack(V(d.begin(), d.end()), buf);
+    using T = std::string::value_type;
+    using V = std::vector< T >;
+    static ByteArray Pack(const std::string& d,
+                          ByteArray buf = ByteArray()) {
+        return SerializeVectorPOD< T >::Pack(V(d.begin(), d.end()), buf);
     }
     static ByteIterator Pack(const std::string &d, ByteIterator bi) {
-        return SerializeVectorPOD::Pack(V(d.begin(), d.end()), bi);
+        return SerializeVectorPOD< T >::Pack(V(d.begin(), d.end()), bi);
     }
     static ConstByteIterator UnPack(ConstByteIterator bi, std::string& d) {
         V v;
-        const bi = SerializeVectorPOD::UnPack(bi, v);
+        bi = SerializeVectorPOD< T >::UnPack(bi, v);
         d = std::string(v.begin(), v.end());
         return bi;
     }
@@ -138,21 +141,44 @@ template < typename T >
 struct InvalidSerializer;
 
 template < typename T >
-struct GetSerializer{
-    using Type = std::conditional< std::is_pod< T >::value,
-                                   SerializePOD< T >,
-                                   Serialize< T > >::type;
+struct GetSerializer< std::vector< T > > {
+    using NCV = typename std::remove_cv< T >::type;
+    using Type = typename std::conditional< std::is_pod< NCV >::value,
+            SerializeVectorPOD< NCV >,
+            SerializeVector< NCV > >::type;
 };
 
 template < typename T >
-struct GetSerializer< std::vector< T > > {
-    using Type = std::conditional< std::is_pod< T >::value,
-                                   SerializeVectorPOD< T >,
-                                   SerializeVector< T > >::type;
+struct GetSerializer< const std::vector< T > >{
+    using Type = typename GetSerializer< std::vector< T > >::Type;
 };
+
+template < typename T >
+struct GetSerializer< volatile std::vector< T > >{
+    using Type = typename GetSerializer< std::vector< T > >::Type;
+};
+
+template < typename T >
+struct GetSerializer{
+    using NCV = typename std::remove_cv< T >::type;
+    using Type = typename std::conditional< std::is_pod< NCV >::value,
+                                   SerializePOD< NCV >,
+                                   Serialize< NCV > >::type;
+};
+
 
 template <>
 struct GetSerializer< std::string > {
+    using Type = SerializeString;
+};
+
+template <>
+struct GetSerializer< const std::string > {
+    using Type = SerializeString;
+};
+
+template <>
+struct GetSerializer< volatile std::string > {
     using Type = SerializeString;
 };
 
--- zcf/SerializerTest.cpp
+++ zcf/SerializerTest.cpp
@@ -16,17 +16,21 @@ int main(int, char**) {
 
     const int intOut = 3;
     using IntSerializer = GetSerializer< decltype(intOut) >::Type;
+    static_assert(std::is_same< IntSerializer, SerializePOD< int > >::value,
+                  "Not SerializePOD< int > type");
     const ByteArray ioutBuf = IntSerializer::Pack(intOut);
     int intIn = -1;
-    IntSerializer::UnPack(ioutBuf, intIn);
+    IntSerializer::UnPack(begin(ioutBuf), intIn);
     assert(intOut == intIn);
 
     const vector< int > vintOut = {1,2,3,4,5,4,3,2,1};
     using VIntSerializer = GetSerializer< decltype(vintOut) >::Type;
-    const ByteArray voutBuf = VIntSerializer::Pack(vinInt);
+    static_assert(std::is_same< VIntSerializer, SerializeVectorPOD< int > >::value,
+                  "Not SerializeVectorPOD< int > type");
+    const ByteArray voutBuf = VIntSerializer::Pack(vintOut);
     vector< int > vintIn;
-    VIntSerializer::UnPack(voutBuf, vintIn);
-    assert(vintIn, vintOut);
+    VIntSerializer::UnPack(begin(voutBuf), vintIn);
+    assert(vintIn == vintOut);
 
     return EXIT_SUCCESS;
 }
